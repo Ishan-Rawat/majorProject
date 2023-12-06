1f3db3af82537449f836ff9f1b22aa44f3f63dbf
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2023-12-06 16:51:50 +0530

    fixed pdf template

--- backend/utils/pdf/pdfTemplate.js
+++ backend/utils/pdf/pdfTemplate.js
@@ -102,7 +102,7 @@ export default function ({
   }
              </div>
              <div class="right">
-                 <h2 style="font-size: 38px; font-weight:200px; font-family: Arial, Helvetica, sans-serif;">${Number(balanceDue) <= 0
+                 <h2 style="font-size: 38px; font-weight:200; font-family: Arial, Helvetica, sans-serif;">${Number(balanceDue) <= 0
    ? "Receipt"
    : document?.documentType
   }</h2>
